72416b3fa365ea107ed050459e1a3b9c1bb015ee
정승현 <tmdgusya@naver.com>
2023-08-02 20:46:52 +0900

    Made Embedding Interface

---
+++ src/embedd/EmbeddingInteface.py
@@ -0,0 +1,17 @@
+
+
+class EmbeddingInterface:
+    """
+    Interface for embedding class
+    You have to implement this method when you make a new embedding class.
+    """
+    
+    def embed(self, text_data):
+        """
+        Embedding text data into vector
+
+        Args:
+            text_data (_type_): Text data to be embedded
+        """
+        pass
+    
