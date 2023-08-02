a61e1a34aac2f65d02682a8d37d5058b572a76c2
정승현 <tmdgusya@naver.com>
2023-08-02 20:30:53 +0900

    Remove argument on Crawler Interface
    - Each Crawler Concrete classes have own crawling logic to get data from External Logic

--- src/crawler/CrawlerInterface.py
+++ src/crawler/CrawlerInterface.py
@@ -6,12 +6,9 @@ class CralwerInterface:
     You have to implement this method when you make a new crawler class.
     """
     
-    def crawl(self, request) -> str:
+    def crawl(self) -> str:
         """
         Load data from external source (e.g. web page, wiki pagem, etc.)
-
-        Args:
-            request (_type_): any information that is needed to crawl data from external source.
 
         Returns:
             str: data from external sources that are converted by only one line.
